7b4ce890fe80fea43dabae379cad41e4549cf2b1
jiangkyd <jiangkyd@yonyou.com>
2021-04-07 11:04:15 +0800

    修改项目结构

---
+++ shop-goods/src/main/java/com/shop/service/impl/GoodsInfoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.shop.service.impl;
+
+import com.shop.mapper.GoodsinfoMapper;
+import com.shop.service.GoodsInfoService;
+
+/**
+ * @author ：jiangkyd
+ * @description：TODO
+ * @date ：2021/4/7 上午9:39
+ */
+public class GoodsInfoServiceImpl implements GoodsInfoService {
+}
