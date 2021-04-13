6e59487d436217e4b540c0681c6e5990ec49533c
jiangkyd <jiangkyd@yonyou.com>
2021-04-13 11:26:47 +0800

    加入CountDownLatch模拟高并发

--- shop-order/src/main/java/com/shop/controller/TestController.java
+++ shop-order/src/main/java/com/shop/controller/TestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -22,6 +23,8 @@ import java.util.concurrent.Future;
 public class TestController {
     @Autowired
     private TestService testService;
+
+
     @RequestMapping("/testOrder")
     public Integer testOrder() throws InterruptedException, ExecutionException {
         List<Future<Integer>> futurepool=new ArrayList<>();
@@ -50,6 +53,44 @@ public class TestController {
                             }
                     }
             }
+        return result;
+    }
+    @RequestMapping("/testOrder2")
+    public Integer testOrder2() throws InterruptedException, ExecutionException {
+        final CountDownLatch latch = new CountDownLatch(200);
+
+        List<Future<Integer>> futurepool=new ArrayList<>();
+        for(int i=1;i<=1000;i++){
+            Future<Integer> future=testService.test2(latch);
+            futurepool.add(future);
+            if(i%200==0){
+                try {
+                    latch.await();//等待CountDownLatch  为0并发执行
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+
+        Integer result=0;
+        while(futurepool.size()>0){
+            Iterator<Future<Integer>> iterable = futurepool.iterator();
+
+            //遍历一遍
+            while(iterable.hasNext()){
+                Future<Integer> future = iterable.next();
+                //如果任务完成取结果，否则判断下一个任务是否完成
+                if (future.isDone() && !future.isCancelled()){
+                    //获取结果
+                    Integer i = future.get();
+                    System.out.println("任务i=" + i + "获取完成，移出任务队列！");
+                    result +=i;
+                    //任务完成移除任务
+                    iterable.remove();
+                }else{
+                    Thread.sleep(1);//避免CPU高速运转，这里休息1毫秒，CPU纳秒级别
+                }
+            }
+        }
         return result;
     }
 }
--- shop-order/src/main/java/com/shop/service/impl/TestService.java
+++ shop-order/src/main/java/com/shop/service/impl/TestService.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
 /**
@@ -36,6 +37,27 @@ public class TestService {
                 log.error(Thread.currentThread().getName()+"----秒杀失败--------");
         }
         log.info("end"+Thread.currentThread().getName()+"-------");
+        return new AsyncResult<>(count);
+    }
+
+    @Async("getExecutor")
+    public Future<Integer> test2(CountDownLatch latch){
+        log.info("start"+Thread.currentThread().getName()+"-------");
+        Integer count=0;
+        try {
+            OrderinfoCuntom orderinfoCuntom=new OrderinfoCuntom(UUID.randomUUID().toString().replace("-", "").toLowerCase());
+            if(orderMainService.addNewSecKillOrder(orderinfoCuntom)) {
+                log.info(Thread.currentThread().getName() + "----秒杀成功1个商品---");
+                count++;
+            }
+            else
+                log.error(Thread.currentThread().getName()+"----秒杀失败--------");
+            log.info("end"+Thread.currentThread().getName()+"-------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            latch.countDown();//线程数-1
+        }
         return new AsyncResult<>(count);
     }
 }
